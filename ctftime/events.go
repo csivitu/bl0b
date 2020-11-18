0dbbd6962e73060d9a839e6ec468ec1ee5e8ee76
Rohan Mukherjee <rohanbayern@gmail.com>
2020-11-18 15:19:54 +0530

    refactor: GetEvents takes time object instead of int64

--- ctftime/events.go
+++ ctftime/events.go
@@ -2,18 +2,22 @@ package ctftime
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/csivitu/bl0b/utils"
 )
 
 // GetEvents returns events between startTime and endTime. The number
 // of events is defined by the value of limit.
-func (ctf *CTFtime) GetEvents(limit int, startTime int64, endTime int64) (Events, error) {
+func (ctf *CTFtime) GetEvents(limit int, startTime time.Time, finishTime time.Time) (Events, error) {
+	start := startTime.Unix()
+	finish := finishTime.Unix()
+
 	endpoint := fmt.Sprintf(
 		"/events/?limit=%d&start=%d&finish=%d",
 		limit,
-		startTime,
-		endTime,
+		start,
+		finish,
 	)
 
 	res, err := ctf.Get(endpoint)
